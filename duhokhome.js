dd01c9e6153e2fbc3715e56b2c8b4200e0205bf6
derfeberwari <160367860+derfeberwari@users.noreply.github.com>
2024-09-23 19:58:30 -0700

    Update duhokhome.js
    json

--- duhokhome.js
+++ duhokhome.js
@@ -3,7 +3,8 @@
  [
      {
     "id": 2,
-    "pe": "https://firebasestorage.googleapis.com/v0/b/fir-auth-c35db.appspot.com/o/wenee%2Fhev.jpg?alt=media&token=3ec01730-ed14-4a38-8f97-ade07e53294b"
+    "pe": "https://firebasestorage.googleapis.com/v0/b/fir-auth-c35db.appspot.com/o/wenee%2Fhev.jpg?alt=media&token=3ec01730-ed14-4a38-8f97-ade07e53294b",
+       "kurdi:"کوردی"
   
   },
   {
