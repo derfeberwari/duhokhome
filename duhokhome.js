8e2f3f2a416fe5ef9cdfb4c42b73b2e0f522cd22
derfeberwari <160367860+derfeberwari@users.noreply.github.com>
2024-09-23 18:19:16 -0700

    Update duhokhome.js
    json

--- duhokhome.js
+++ duhokhome.js
@@ -1,6 +1,11 @@
 {
   "Duhok":
  [
+     {
+    "id": 2,
+    "pe": "https://firebasestorage.googleapis.com/v0/b/fir-auth-c35db.appspot.com/o/wenee%2Fqw.jpg?alt=media&token=e98a08ee-8f35-4c82-9707-31c1cea83c66",
+    "isFriendly": true
+  }
   {
     "id": 1,
     "pe": "https://firebasestorage.googleapis.com/v0/b/fir-auth-c35db.appspot.com/o/wenee%2Fqw.jpg?alt=media&token=e98a08ee-8f35-4c82-9707-31c1cea83c66",
