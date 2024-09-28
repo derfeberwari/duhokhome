342baa4bb613bf5fa257b541cc2678306c60c1bc
derfeberwari <160367860+derfeberwari@users.noreply.github.com>
2024-09-28 06:57:38 -0700

    Update duhokhome.js
    json

--- duhokhome.js
+++ duhokhome.js
@@ -9,7 +9,8 @@
     "ruj":"ئەینی",
     "tarix":"2024/09/20",
     "t1":"https://firebasestorage.googleapis.com/v0/b/fir-auth-c35db.appspot.com/o/wenee%2FIMG_8241.PNG?alt=media&token=711c5dac-0f3a-42ff-bb92-991103845698",
-    "nt1":" دهۆک"
+    "nt1":" دهۆک",
+    "wext":"0-1"
    
   }
  
