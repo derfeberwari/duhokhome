276d7ff3937e19ac3a1f1811ba0e8da7616894e8
derfeberwari <160367860+derfeberwari@users.noreply.github.com>
2024-09-28 06:51:13 -0700

    Update duhokhome.js
    json

--- duhokhome.js
+++ duhokhome.js
@@ -8,7 +8,8 @@
     "logo":"https://firebasestorage.googleapis.com/v0/b/fir-auth-c35db.appspot.com/o/wenee%2Flogoiraq.PNG?alt=media&token=0967cd50-bd97-4a83-854e-5659dab54613",
     "ruj":"ئەینی",
     "tarix":"2024/09/20",
-    "pe": "https://firebasestorage.googleapis.com/v0/b/fir-auth-c35db.appspot.com/o/wenee%2Fqw.jpg?alt=media&token=e98a08ee-8f35-4c82-9707-31c1cea83c66"
+    "t1":"https://firebasestorage.googleapis.com/v0/b/fir-auth-c35db.appspot.com/o/wenee%2FIMG_8241.PNG?alt=media&token=711c5dac-0f3a-42ff-bb92-991103845698",
+    "nt1":" دهۆک"
    
   }
  
