b422f71d2da8caaf77629d90f9a140010aaf2ec3
derfeberwari <160367860+derfeberwari@users.noreply.github.com>
2024-09-28 09:42:56 -0700

    Update duhokhome.js
    json

--- duhokhome.js
+++ duhokhome.js
@@ -15,6 +15,19 @@
      "t2":"https://firebasestorage.googleapis.com/v0/b/fir-auth-c35db.appspot.com/o/wenee%2Fduhok%2Fzawra-removebg-preview.png?alt=media&token=9d3192dd-3a32-4b43-9a72-630a0a8265f7"
    
   },
+    {
+    "id": 1,
+    "ger":"گەڕا ئێکێ ژ خولا ستێرێن ئێراقێ",
+    "logo":"https://firebasestorage.googleapis.com/v0/b/fir-auth-c35db.appspot.com/o/wenee%2Flogoiraq.PNG?alt=media&token=0967cd50-bd97-4a83-854e-5659dab54613",
+    "ruj":"ئەینی",
+    "tarix":"2024/09/20",
+    "t1":"https://firebasestorage.googleapis.com/v0/b/fir-auth-c35db.appspot.com/o/wenee%2FIMG_8241.PNG?alt=media&token=711c5dac-0f3a-42ff-bb92-991103845698",
+    "nt1":" دهۆک",
+    "wext":"0-1",
+    "nt2":"زەوڕا",
+     "t2":"https://firebasestorage.googleapis.com/v0/b/fir-auth-c35db.appspot.com/o/wenee%2Fduhok%2Fzawra-removebg-preview.png?alt=media&token=9d3192dd-3a32-4b43-9a72-630a0a8265f7"
+   
+  },
      {
     "id": 2,
     "ger":"گەڕا دووێ ژ خولا ستێرێن ئێراقێ",
