d184cbb326b198dd3d2c7d22982c3e17c6cc037c
derfeberwari <160367860+derfeberwari@users.noreply.github.com>
2024-09-28 04:08:07 -0700

    Update duhokhome.js
    json

--- duhokhome.js
+++ duhokhome.js
@@ -1,18 +1,7 @@
 {
   "Duhok":
  [
-     {
-    "id": 2,
-    "pe": "https://firebasestorage.googleapis.com/v0/b/fir-auth-c35db.appspot.com/o/wenee%2Fhev.jpg?alt=media&token=3ec01730-ed14-4a38-8f97-ade07e53294b",
-       "kurdi":"کوردی",
-       "badini":"ئه‌و ياريكه‌رێ پێشتر ئه‌رسینال نه‌شیایی ببه‌ت دهۆكێ بده‌ستڤه‌ ئینا یاریكه‌رێ پێشتر یێ یانه‌یا برایتون یا ئنگلیزی و یانه‌یێن مالمو و ئوریبورو یێن سویدی پیته‌ر گورگیس دهۆكا داسنیا هه‌لبژارت .",
-       "arabic":"عربي",
-        "arabicc":"اللاعب اللذي لم يظفر ارسنال بجهوده ظفر به نادي دهوكتعاقد نادي دهوك مع لاعب نادي اندية برايتون الإنكليزي و مالمو و اوريبرو السويديين والمنتخب العراقي پيتر گورگيس .",
-       "english":"English",
-       "englishh":"The player Arsenal FC couldn't secure is now joining Duhok FC! The former player of English Brighton FC, Swedish clubs Malmo and Örebro, and the Iraq National Football Team, Peter Gwargis, has chosen Duhok FC as his next destination."
-       
   
-  },
   {
     "id": 1,
     "pe": "https://firebasestorage.googleapis.com/v0/b/fir-auth-c35db.appspot.com/o/wenee%2Fqw.jpg?alt=media&token=e98a08ee-8f35-4c82-9707-31c1cea83c66"
