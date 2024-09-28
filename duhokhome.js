bd358379ebb7bb390d8e7d47163019e2ce08fc5f
derfeberwari <160367860+derfeberwari@users.noreply.github.com>
2024-09-28 07:29:42 -0700

    Update duhokhome.js
    json

--- duhokhome.js
+++ duhokhome.js
@@ -12,8 +12,7 @@
     "nt1":" دهۆک",
     "wext":"0-1",
     "nt2":"زەوڕا",
-     "t2":"https://firebasestorage.googleapis.com/v0/b/fir-auth-c35db.appspot.com/o/wenee%2Fduhok%2Fzawra-removebg-preview.png?alt=media&token=9d3192dd-3a32-4b43-9a72-630a0a8265f7",
-    
+     "t2":"https://firebasestorage.googleapis.com/v0/b/fir-auth-c35db.appspot.com/o/wenee%2Fduhok%2Fzawra-removebg-preview.png?alt=media&token=9d3192dd-3a32-4b43-9a72-630a0a8265f7"
    
   }
  
